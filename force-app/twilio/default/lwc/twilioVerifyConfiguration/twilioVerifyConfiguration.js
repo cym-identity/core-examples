5bdf0d32e6840e3ce5b17ac839b28b431ea501df
Youssef Kefal <youssef.kefal@gmail.com>
2021-04-04 21:13:56 +0200

    twilio UI

---
+++ force-app/twilio/default/lwc/twilioVerifyConfiguration/twilioVerifyConfiguration.js
@@ -0,0 +1,31 @@
+import { LightningElement, api, wire } from 'lwc';
+import remoteSiteSettingEnabled from '@salesforce/apex/TwilioVerify.is_twilio_reachable';
+
+export default class TwilioVerifyConfiguration extends LightningElement {
+  @api save;
+  @api config;
+
+  is_remote_site_setting_enabled;
+
+  @wire(remoteSiteSettingEnabled)
+  getRemoteSiteSettingEnabled(resp) {
+    const {error, data} = resp;
+    this.is_remote_site_setting_enabled = data === true;
+  }
+
+  handleClick() {
+    this.save(this.config);
+  }
+
+  handleAccountSIDChange(e) {
+    this.config = Object.assign({}, this.config, {account_sid: e.target.value});
+  }
+
+  handleAuthTokenChange(e) {
+    this.config = Object.assign({}, this.config, {auth_token: e.target.value});
+  }
+
+  handleServiceSIDChange(e) {
+    this.config = Object.assign({}, this.config, {service_sid: e.target.value});
+  }
+}
